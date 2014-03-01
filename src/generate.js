676e5b815949d98ca17afe0e55306f34031b1ab9
TheUnknownOne <theunknownone25@gmail.com>
2014-03-02 00:47:15 +0100

    New insults

--- src/generate.js
+++ src/generate.js
@@ -25,11 +25,11 @@ insults = removeDuplicates(insults);
 
 // Remove \n in the last line (for the json) - add it back in the source
 insults[insults.length - 2] = insults[insults.length - 2].replace('\\n', '');
-insults.splice(insults.length - 1, 1);
+//insults.splice(insults.length - 1, 1);
 
 // Generate indented JSON
 json = JSON.stringify(insults, null, 4);
 
 // Write to files
-fs.writeFileSync('insults.txt', insults.join('\n\n') + '\n', {encoding: 'utf8'});
+fs.writeFileSync('insults.txt', insults.join('\n\n'), {encoding: 'utf8'});
 fs.writeFileSync('insults.json', json, {encoding: 'utf8'});
